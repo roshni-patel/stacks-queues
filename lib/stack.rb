7482638c04bc7106e0e4a4eb5b7a6f0c1255be56
roshni-patel <rpatel01@wesleyan.edu>
2021-04-10 16:20:30 -0500

    Added methods for implementing stack using provided linked list

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,19 +1,20 @@
 class Stack
   def initialize
-    # @store = ...
-    raise NotImplementedError, "Not yet implemented"
+    @store = LinkedList.new
   end
 
   def push(element)
-    raise NotImplementedError, "Not yet implemented"
+    @store.add_first(element)
   end
 
   def pop
-    raise NotImplementedError, "Not yet implemented"
+    return nil if self.empty?
+    item = @store.remove_first
+    return item
   end
 
   def empty?
-    raise NotImplementedError, "Not yet implemented"
+    return @store.empty?
   end
 
   def to_s
